32dc2069fd8702f64d95560e45d6120ec726aee8
Yeraldin Urbina <yeriisurbina54@gmail.com>
2024-11-01 21:11:53 -0500

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Home.css';
 
-function Home() {
+const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-container">
+      <header className="hero-section">
+        <h1>Explorando el sistema solar </h1>
+      </header>
 
-export default Home
+      <section className="history-section">
+        <h1>Nuestra Historia</h1>
+        <p>
+          Esta es una página que utiliza la API de la NASA, la cual está
+          relacionada con Marte. Esta API está diseñada para recopilar datos de
+          imágenes obtenidas por los rovers Curiosity, Opportunity y Spirit de
+          la NASA en Marte, facilitando su acceso a desarrolladores, educadores
+          y científicos ciudadanos.
+        </p>
+        <p>
+          El propósito de esta página es apoyar los procesos educativos
+          relacionados con la ciencia y fomentar la curiosidad de los
+          estudiantes sobre el espacio. A través de esta plataforma, buscamos
+          proporcionar recursos que inspiren a los estudiantes a explorar y
+          aprender más sobre el fascinante mundo de la exploración espacial.
+        </p>
+      </section>
+
+      <footer className="footer-section">
+        <p>© 2024 Explorador del sistema solar. Todos los derechos reservados.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
