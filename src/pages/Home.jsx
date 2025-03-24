a69e16014519697f9f99c76aeb31941be55c3610
KdnaStone <72689531+Kdnastone@users.noreply.github.com>
2025-03-23 19:09:21 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,15 +17,16 @@ const Home = () => {
           Esta es una página que utiliza la API de la NASA, la cual está
           relacionada con Marte. Esta API está diseñada para recopilar datos de
           imágenes obtenidas por los rovers Curiosity, Opportunity y Spirit de
-          la NASA en Marte, facilitando su acceso a desarrolladores, educadores
-          y científicos ciudadanos.
+          la NASA en Marte, facilitando su acceso a estudiantes, educadores,
+          y ciudadanos.
         </p>
         <p>
           El propósito de esta página es apoyar los procesos educativos
           relacionados con la ciencia y fomentar la curiosidad de los
-          estudiantes sobre el espacio. A través de esta plataforma, buscamos
-          proporcionar recursos que inspiren a los estudiantes a explorar y
-          aprender más sobre el fascinante mundo de la exploración espacial.
+          estudiantes sobre el espacio. 
+          A través de esta plataforma, buscamos proporcionar recursos que 
+          inspiren a los estudiantes a explorar y aprender más sobre el 
+          fascinante mundo de la exploración espacial.
         </p>
       </section>
 
